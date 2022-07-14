684bee1ca4cf88dfc3b8671b6960258fa990fd56
Tony Grimes <acidtone@tonygrimes.com>
2022-07-14 17:27:54 -0600

    broken - trasfer to Karnak

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -16,9 +16,9 @@
     Skip to main content
   </a>
   
-  <a href="/" class="site-title">
+  <!-- <a href="/" class="site-title">
     {siteTitle}
-  </a>
+  </a> -->
   
   <HamburgerMenuButton />
   <MainNav />
